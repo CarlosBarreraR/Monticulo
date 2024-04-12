01b03d0d521e3f9c0abd6a9f9a1616ac3b53c24d
CarlosBarreraR <159270162+CarlosBarreraR@users.noreply.github.com>
2024-04-11 23:39:45 -0500

    ServicioPolicia.py

--- ServicioPolicia.py
+++ ServicioPolicia.py
@@ -1,82 +1,25 @@
-class Nodo:
-    def __init__(self, dato):
-        self.dato = dato
-        self.izquierda = None
-        self.derecha = None
-        
-class Arbol:
-    # Funciones privadas
-    def __init__(self, dato):
-        self.raiz = Nodo(dato)
+class Persona:
+    def __init__(self, nombre, edad, direccion, motivo, gravedad):
+        self.nombre = nombre
+        self.edad = edad
+        self.direccion = direccion
+        self.motivo = motivo
+        self.gravedad = gravedad
 
-    def __agregar_recursivo(self, nodo, dato):
-        if dato < nodo.dato:
-            if nodo.izquierda is None:
-                nodo.izquierda = Nodo(dato)
-            else:
-                self.__agregar_recursivo(nodo.izquierda, dato)
-        else:
-            if nodo.derecha is None:
-                nodo.derecha = Nodo(dato)
-            else:
-                self.__agregar_recursivo(nodo.derecha, dato)
+    def __lt__(self, other):
+        if self.gravedad == other.gravedad:
+            return self.edad < other.edad
+        return self.gravedad < other.gravedad
 
-    def __inorden_recursivo(self, nodo):
-        if nodo is not None:
-            self.__inorden_recursivo(nodo.izquierda)
-            print(nodo.dato, end=", ")
-            self.__inorden_recursivo(nodo.derecha)
-
-    def __preorden_recursivo(self, nodo):
-        if nodo is not None:
-            print(nodo.dato, end=", ")
-            self.__preorden_recursivo(nodo.izquierda)
-            self.__preorden_recursivo(nodo.derecha)
-
-    def __postorden_recursivo(self, nodo):
-        if nodo is not None:
-            self.__postorden_recursivo(nodo.izquierda)
-            self.__postorden_recursivo(nodo.derecha)
-            print(nodo.dato, end=", ")
-
-    def __buscar(self, nodo, busqueda):
-        if nodo is None:
-            return None
-        if nodo.dato == busqueda:
-            return nodo
-        if busqueda < nodo.dato:
-            return self.__buscar(nodo.izquierda, busqueda)
-        else:
-            return self.__buscar(nodo.derecha, busqueda)
-
-    # Funciones públicas
-
-    def agregar(self, dato):
-        self.__agregar_recursivo(self.raiz, dato)
-
-    def inorden(self):
-        print("Imprimiendo árbol inorden: ")
-        self.__inorden_recursivo(self.raiz)
-        print("")
-
-    def preorden(self):
-        print("Imprimiendo árbol preorden: ")
-        self.__preorden_recursivo(self.raiz)
-        print("")
-
-    def postorden(self):
-        print("Imprimiendo árbol postorden: ")
-        self.__postorden_recursivo(self.raiz)
-        print("")
-
-    def buscar(self, busqueda):
-        return self.__buscar(self.raiz, busqueda)
-    
 class MonticuloBinario:
     def __init__(self):
         self.listaMonticulo = [0]
-        self.tamanoActual = 0
+        self.cola = 0
 
+    def insertar(self,persona):
+      self.listaMonticulo.append(persona)
+      self.cola = self.cola + 1
+      self.infiltArriba(self.cola)
 
     def infiltArriba(self,i):
         while i // 2 > 0:
@@ -86,13 +29,16 @@ class MonticuloBinario:
              self.listaMonticulo[i] = tmp
           i = i // 2
 
-    def insertar(self,k):
-      self.listaMonticulo.append(k)
-      self.tamanoActual = self.tamanoActual + 1
-      self.infiltArriba(self.tamanoActual)
+    def eliminarMin(self):
+        valorSacado = self.listaMonticulo[1]
+        self.listaMonticulo[1] = self.listaMonticulo[self.cola]
+        self.cola = self.cola - 1
+        self.listaMonticulo.pop()
+        self.infiltAbajo(1)
+        return valorSacado
 
     def infiltAbajo(self,i):
-      while (i * 2) <= self.tamanoActual:
+      while (i * 2) <= self.cola:
           hm = self.hijoMin(i)
           if self.listaMonticulo[i] > self.listaMonticulo[hm]:
               tmp = self.listaMonticulo[i]
@@ -101,58 +47,13 @@ class MonticuloBinario:
           i = hm
 
     def hijoMin(self,i):
-      if i * 2 + 1 > self.tamanoActual:
+      if i * 2 + 1 > self.cola:
           return i * 2
       else:
           if self.listaMonticulo[i*2] < self.listaMonticulo[i*2+1]:
               return i * 2
           else:
               return i * 2 + 1
-
-    def eliminarMin(self):
-      valorSacado = self.listaMonticulo[1]
-      self.listaMonticulo[1] = self.listaMonticulo[self.tamanoActual]
-      self.tamanoActual = self.tamanoActual - 1
-      self.listaMonticulo.pop()
-      self.infiltAbajo(1)
-      return valorSacado
-
-    def construirMonticulo(self,unaLista):
-      i = len(unaLista) // 2
-      self.tamanoActual = len(unaLista)
-      self.listaMonticulo = [0] + unaLista[:]
-      while (i > 0):
-          self.infiltAbajo(i)
-          i = i - 1
-
-miMonticulo = MonticuloBinario()
-miMonticulo.construirMonticulo([9,5,6,2,3])
-
-print(miMonticulo.eliminarMin())
-print(miMonticulo.eliminarMin())
-print(miMonticulo.eliminarMin())
-print(miMonticulo.eliminarMin())
-print(miMonticulo.eliminarMin())
-
-class Persona:
-    def __init__(self, nombre, edad, direccion, motivo, gravedad):
-        self.nombre = nombre
-        self.edad = edad
-        self.direccion = direccion
-        self.motivo = motivo
-        self.gravedad = gravedad
-
-        if edad < 12:
-            self.prioridad = 1
-        elif edad > 65:
-            self.prioridad = 2
-        else:
-            self.prioridad = 4
-
-    def __lt__(self, other):
-        if self.gravedad == other.gravedad:
-            return self.prioridad < other.prioridad
-        return self.gravedad < other.gravedad
 
 class ServicioPolicial:
     def __init__(self):
@@ -165,14 +66,16 @@ class ServicioPolicial:
         return self.cola.eliminarMin()
 
     def mostrar_cola(self):
-        return self.cola.listaMonticulo[1:]
+        return self.cola.cola
 
 servicio = ServicioPolicial()
+
 while True:
     print("1. Ingresar Llamada")
     print("2. Pasar siguiente solicitud")
     print("3. Mostrar la cola")
     opcion = int(input("Seleccione una opción: "))
+
     if opcion == 1:
         nombre = input("Ingrese el nombre: ")
         edad = int(input("Ingrese la edad: "))
@@ -181,10 +84,23 @@ while True:
         gravedad = int(input("Ingrese la gravedad (1-5): "))
         llamada = Persona(nombre, edad, direccion, motivo, gravedad)
         servicio.ingresar_llamada(llamada)
+        cola = servicio.mostrar_cola()
+        if cola:
+            print(f"{llamada.nombre} será atendido en la posición {posicion}")
+
+
     elif opcion == 2:
         siguiente = servicio.pasar_siguiente()
-        print(f"La siguiente solicitud es de {siguiente.nombre}")
+        if siguiente:
+            print(f"La siguiente solicitud es de {siguiente.nombre}")
+        else:
+            print("No hay más solicitudes pendientes.")
+
     elif opcion == 3:
         cola = servicio.mostrar_cola()
-        for llamada in cola:
-            print(f"{llamada.nombre}, {llamada.edad}, {llamada.direccion}, {llamada.motivo}, {llamada.gravedad}")
+        if cola:
+            for posicion, llamada in enumerate(cola, start=1):
+                print(f"\n{posicion}. {llamada.nombre}, {llamada.edad}, {llamada.direccion}, {llamada.motivo}, {llamada.gravedad}\n")
+        else:
+            print("La cola está vacía.")
+            
